94e77944579811b23aeb62ae1208d1a2e6321c1d
murtzoyevolimjon734@gmail.com <murtazoyevolimjon734@gmail.com>
2024-12-25 18:10:09 +0500

    ✅DONE

---
+++ task07.py
@@ -0,0 +1,7 @@
+word = str(input("Biror matn kiriting: "))#Xohlagan matn kiriting
+
+search = str(input("Izlayotgan so'zingiz: "))#izlayotgan so'zingizni kiriting
+
+search_word = search in word #bor yuqligini tekshirish
+
+print(search_word)
