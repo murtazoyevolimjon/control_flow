bfd7b21e9e0165e32dc298ce004fe1eb2dc22a3b
murtzoyevolimjon734@gmail.com <murtazoyevolimjon734@gmail.com>
2024-12-25 17:01:18 +0500

    ✅DONE

---
+++ task04.py
@@ -0,0 +1,7 @@
+age = int(input("Yoshingizni kiriting: "))#istalgan yoshingizni kiriting
+#voyaga yetganligi haqida malumot
+if age <= 18:
+
+        print("Voyaga yetgan ")
+    else:
+        print("Voyaga yetmagan ")
