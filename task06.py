44211cf9f721f1280496fceeb7d8b0ae077e2f45
murtzoyevolimjon734@gmail.com <murtazoyevolimjon734@gmail.com>
2024-12-25 18:00:13 +0500

    ✅DONE

---
+++ task06.py
@@ -0,0 +1,7 @@
+word = str (input("Biror bir matn kiritinig "))
+# Foydalanuvchi kiritgan matni bosh harflarni katta harfga o'zgartirildi
+new_word = word.title()
+# bajarilgan amallarni terminlaga chiqarishini suradik
+print("Siz kiritgan matn bosh harflari katta harfga o'zgartirildi ")
+print (new_word)
+
