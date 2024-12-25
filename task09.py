4f5265a5d712fadf913d90dec3bc6aca6a9c5d86
murtzoyevolimjon734@gmail.com <murtazoyevolimjon734@gmail.com>
2024-12-25 18:33:16 +0500

    ✅DONE

---
+++ task09.py
@@ -0,0 +1,8 @@
+number = int(input("Son kiriting: "))#xohlagan soningizni kiriting
+count = 0
+result = 0
+while count < number:
+    count += 1
+    result += count
+
+print("Natija== ",result)#0 dan oxirgi songacha bo'lgan sonlarni yig'insini topish
