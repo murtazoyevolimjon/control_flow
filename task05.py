3d1622aab46dcbcb93f58d6cef51489cd6a676a8
murtzoyevolimjon734@gmail.com <murtazoyevolimjon734@gmail.com>
2024-12-25 17:20:30 +0500

    ✅DONE

---
+++ task05.py
@@ -0,0 +1,7 @@
+matn = input("Matn kiriting: ")#Xohlagan matnizni kiriting
+
+count = int(input("Necha marta takrorlash kearak: "))#necha marta takrorlanishi kerak
+i = 0
+while i < count:
+    print(i + 1, '-', matn)
+    i += 1
